98a6cb1e296273fca94af3695bc18c53adc46db2
niklhut <49069026+niklhut@users.noreply.github.com>
2024-11-09 20:25:54 +0100

    Initial template release

--- src/main/java/h05/Airspace.java
+++ src/main/java/h05/Airspace.java
@@ -8,30 +8,30 @@ public class Airspace {
     private static Airspace INSTANCE = null;
 
 
-    public static Airspace get(){
-        if(INSTANCE == null){
+    public static Airspace get() {
+        if (INSTANCE == null) {
             INSTANCE = new Airspace();
         }
         return INSTANCE;
     }
 
+    // TODO: H5.1.1 - Uncomment the following lines
+//    private final Set<Flying> flyingInAirspace = new HashSet<>();
+//
+//    private Airspace(){
+//
+//    }
+//
+//    void register(Flying flying){
+//        flyingInAirspace.add(flying);
+//    }
+//
+//    void deregister(Flying flying){
+//        flyingInAirspace.remove(flying);
+//    }
 
-    private final Set<Flying> flyingInAirspace = new HashSet<>();
 
-    private Airspace(){
-
-    }
-
-    void register(Flying flying){
-        flyingInAirspace.add(flying);
-    }
-
-    void deregister(Flying flying){
-        flyingInAirspace.remove(flying);
-    }
-
-
-    void scanAirspace(){
-        // TODO: Student Implementation
+    void scanAirspace() {
+        // TODO: H5.5
     }
 }
